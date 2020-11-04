ecd4e12c354a91e4c7ffec680ec5dd126fdb2ccf
Jiyun Lee <qlrqod026@naver.com>
2020-11-04 15:34:34 +0900

    update

---
+++ js/waveGroup.js
@@ -0,0 +1,46 @@
+import { Wave } from "./wave.js";
+
+export class WaveGroup {
+    constructor() {
+        this.totalWaves = 3;
+        this.totalPoints = 7;
+
+        this.color = ['rgba(255,0,0,0.4)','rgba(0,255,255,0.4)','rgba(255,255,0,0.4)'];
+
+        this.waves = [];
+
+        for (let i = 0; i < this.totalWaves; i++){
+            const wave = new Wave(
+                i,
+                this.totalPoints,
+                this.color[i]
+
+            );
+            this.waves[i] = wave;
+
+        }
+    }//wavegroup
+
+
+    resize(stageWidth, stageHeight) {
+        for (let i = 0; i < this.totalWaves; i++){
+            const wave = this.waves[i]
+            wave.resize(stageWidth, stageHeight);
+
+        }
+    }//resize
+
+
+
+    draw(ctx) {
+        for (let i = 0; i < this.totalWaves; i++ ){
+            const wave = this.waves[i]
+            wave.draw(ctx);
+
+        }
+
+    }
+
+
+
+}
